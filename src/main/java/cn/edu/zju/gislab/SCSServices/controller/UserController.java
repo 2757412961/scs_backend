cbfd068c3df60dab95f7ed12ba83451efadf56a1
2757412961 <39891566+2757412961@users.noreply.github.com>
2020-11-17 21:30:34 +0800

    添加logback日志记录

--- src/main/java/cn/edu/zju/gislab/SCSServices/controller/UserController.java
+++ src/main/java/cn/edu/zju/gislab/SCSServices/controller/UserController.java
@@ -2,51 +2,74 @@ package cn.edu.zju.gislab.SCSServices.controller;
 
 import cn.edu.zju.gislab.SCSServices.po.ScsUsers;
 import cn.edu.zju.gislab.SCSServices.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.*;
+import java.net.HttpCookie;
+import java.util.Enumeration;
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
 
-    //登录验证
+    // 登录验证
     @RequestMapping("/login")
-    public @ResponseBody int login(String username, String password) {
+    public int login(String username, String password) {
         // 登录验证
         int loginResult = userService.checkLogin(username, password);
         return loginResult;
     }
-    //查询所有用户
+
+    // 查询所有用户
     @RequestMapping("/AllUsers")
-    public @ResponseBody List<ScsUsers> queryAllUser() {
+    public List<ScsUsers> queryAllUser() {
         List<ScsUsers> result = userService.getAllUsers();
         return result;
     }
-    //
+
     @RequestMapping("/updateUser")
-    public @ResponseBody int updateUser(String username,String password,int category) {
-        int result = userService.updateUser(username,password,category);
+    public int updateUser(String username, String password, int category) {
+        int result = userService.updateUser(username, password, category);
         return result;
     }
-    //
+
     @RequestMapping("/deleteUser")
-    public @ResponseBody int deleteUser(String username) {
+    public int deleteUser(String username) {
         int result = userService.deleteUser(username);
         return result;
     }
-    //
+
     @RequestMapping("/addUser")
-    public @ResponseBody int addUser(String username,String password,int groupId) {
-        int result = userService.addUser(username,password,groupId);
+    public int addUser(String username, String password, int groupId) {
+        int result = userService.addUser(username, password, groupId);
         return result;
+    }
+
+    // 曲终人散
+    @RequestMapping("/userLogin")
+    public Enumeration<String> userLogin(String username, String password,
+                                         HttpSession session,
+                                         HttpServletRequest request, HttpServletResponse response) {
+
+//        session.setAttribute("username", username);
+//        session.setAttribute("password", password);
+
+        logger.info("Hello world!");
+        // 默认情况下debug不会显示
+        logger.info("info日志");
+        logger.debug("debug日志");
+        logger.error("error日志");
+        logger.warn("warn日志");
+
+        return session.getAttributeNames();
     }
 
 }
